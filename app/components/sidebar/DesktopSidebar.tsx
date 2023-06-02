3e3403c81a0649523c1d6bc17fb16fd35f90c768
CarlosGozalbes <carlosfgmendoza@gmail.com>
2023-06-02 18:01:35 +0200

    users lists avatar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,16 +1,22 @@
-'use client'
+"use client";
 
-import useRoutes from "@/app/hooks/useRoutes"
-import { useState } from "react"
-import DesktopItem from "./DesktopItem"
+import useRoutes from "@/app/hooks/useRoutes";
+import { useState } from "react";
+import DesktopItem from "./DesktopItem";
+import { User } from "@prisma/client";
+import Avatar from "../Avatar";
 
+interface DesktopSidebarProps {
+  currentUser: User
+}
 
-const DesktopSidebar =()=> {
-    const routes = useRoutes()
-    const [isOpen, setIsOpen] = useState(false)
-    return (
-      <div
-        className="hidden 
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
+  const routes = useRoutes();
+  const [isOpen, setIsOpen] = useState(false);
+  console.log({ currentUser });
+  return (
+    <div
+      className="hidden 
         lg:fixed 
         lg:inset-y-0 
         lg:left-0 
@@ -24,22 +30,30 @@ const DesktopSidebar =()=> {
         lg:flex
         lg:flex-col
         justify-between"
-      >
-        <nav className="mt-4 flex flex-col justify-between">
-          <ul role="list" className="flex flex-col items-center space-y-1">
-            {routes.map((item) => (
-              <DesktopItem
-                key={item.label}
-                href={item.href}
-                label={item.label}
-                icon={item.icon}
-                active={item.active}
-                onClick={item.onClick}
-              />
-            ))}
-          </ul>
-        </nav>
-      </div>
-    );
-}
-export default DesktopSidebar
+    >
+      <nav className="mt-4 flex flex-col justify-between">
+        <ul role="list" className="flex flex-col items-center space-y-1">
+          {routes.map((item) => (
+            <DesktopItem
+              key={item.label}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+              active={item.active}
+              onClick={item.onClick}
+            />
+          ))}
+        </ul>
+      </nav>
+      <nav className="mt-4 flex flex-col justify-between items-center">
+        <div
+          onClick={() => setIsOpen(true)}
+          className="cursor-pointer hover:opacity-75 transition"
+        >
+          <Avatar user={currentUser} />
+        </div>
+      </nav>
+    </div>
+  );
+};
+export default DesktopSidebar;
--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,12 +1,15 @@
+import getUsers from '../actions/getUsers'
 import Sidebar from '../components/sidebar/Sidebar'
 
 export default async function UsersLayout({children}:{
     children:React.ReactNode
 }) {
+    const users = await getUsers()
     return (
         // @ts-expect-error Server Component
         <Sidebar>
         <div className="h-full">
+            <UserList items={users}/>
             {children}
         </div>
         </Sidebar>
